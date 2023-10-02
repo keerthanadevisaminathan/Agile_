3976d7826d211981b2de12fe8e4d9dacd5e1af83
keerthanadevisaminathan <128278164+keerthanadevisaminathan@users.noreply.github.com>
2023-10-02 08:05:43 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {Routes, Route} from 'react-router-dom';
+import Table from './Table';
+import Home from './Home';
+import UiDesign from './Datainsertion'
+import Docappointment from './Docappointment';
+import Adminlogin from './Adminlogin';
+import Myform from './Myform.js';
+import Adminpage from './Adminpage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Userform/> */}
+      { <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+      
+        <Route path='/Table' element={<Table/>}></Route>
+        <Route path='/Myform' element={<Myform/>}></Route>
+        <Route path='/UiDesign' element={<UiDesign/>}></Route>
+        <Route path='/Adminlogin' element={<Adminlogin/>}></Route>
+        <Route path='/Adminpage' element={<Adminpage/>}></Route>
+        <Route path='/Docappointment' element={<Docappointment/>}></Route>
+      </Routes> }
     </div>
   );
 }
